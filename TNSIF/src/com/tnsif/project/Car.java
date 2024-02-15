6863a0da96de83c0d73d7c7ac6715fc6e42d39a8
7JyothsnaMercy <jyothsnamercysuraboina@gmail.com>
2024-02-15 10:15:21 +0530

    First Message

---
+++ TNSIF/src/com/tnsif/project/Car.java
@@ -0,0 +1,27 @@
+package com.tnsif.project;
+
+public class Car {
+	String name;
+	int id;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+               	Car obj1= new Car();
+               	Car obj2= new Car();
+               	
+				obj1.id=1;
+				obj1.name="Jyothsna";
+				System.out.println(obj1.name);
+				System.out.println(obj1.id);
+				
+				obj2.id=1;
+				obj2.name="Mercy";
+				System.out.println(obj2.name);
+				System.out.println(obj2.id);
+			}
+
+
+	
+
+}
